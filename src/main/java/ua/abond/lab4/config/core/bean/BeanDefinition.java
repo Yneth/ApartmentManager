81795471e1300774eb316889edc632f793b89b1b
Anthony <zaknofein2@gmail.com>
2016-11-28 21:20:03 +0200

    BeanDefinition now stores metadata about declaring class factory method (it is the case when there is a method that was annotated with Bean annotation in a Component class)

--- src/main/java/ua/abond/lab4/config/core/bean/BeanDefinition.java
+++ src/main/java/ua/abond/lab4/config/core/bean/BeanDefinition.java
@@ -1,13 +1,23 @@
 package ua.abond.lab4.config.core.bean;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 
 public class BeanDefinition implements Comparable<BeanDefinition> {
     private final Class<?> type;
 
+    private Class<?> declaringClass;
+    private Method factoryMethod;
+
     public BeanDefinition(Class<?> type) {
         this.type = type;
+    }
+
+    public BeanDefinition(Class<?> type, Method factoryMethod, Class<?> declaringClass) {
+        this.type = type;
+        this.factoryMethod = factoryMethod;
+        this.declaringClass = declaringClass;
     }
 
     public boolean isAbstract() {
@@ -23,7 +33,21 @@ public class BeanDefinition implements Comparable<BeanDefinition> {
         if (constructors.length > 1) {
             return false;
         }
-        return constructors.length == 0 || constructors[0].getParameterCount() == 0;
+        return constructors.length == 0
+                || constructors[0].getParameterCount() == 0
+                || (hasFactoryMethod() && factoryMethod.getParameterCount() == 0);
+    }
+
+    public boolean hasFactoryMethod() {
+        return factoryMethod != null;
+    }
+
+    public Method getFactoryMethod() {
+        return factoryMethod;
+    }
+
+    public Class<?> getDeclaringClass() {
+        return declaringClass;
     }
 
     public Class<?> getType() {
