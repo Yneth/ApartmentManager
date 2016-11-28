39908cd1e8aac2692579734f7622b627ef3bc93e
Anthony <zaknofein2@gmail.com>
2016-11-28 21:22:48 +0200

    Changed definition of InjectABPP

--- src/main/java/ua/abond/lab4/config/core/bean/InjectAnnotationBeanPostProcessor.java
+++ src/main/java/ua/abond/lab4/config/core/bean/InjectAnnotationBeanPostProcessor.java
@@ -1,8 +1,8 @@
 package ua.abond.lab4.config.core.bean;
 
-import ua.abond.lab4.config.core.ApplicationContext;
-import ua.abond.lab4.config.core.ApplicationContextAware;
+import ua.abond.lab4.config.core.BeanFactory;
 import ua.abond.lab4.config.core.BeanPostProcessor;
+import ua.abond.lab4.config.core.ConfigurableBeanFactory;
 import ua.abond.lab4.config.core.Ordered;
 import ua.abond.lab4.config.core.annotation.Inject;
 import ua.abond.lab4.config.core.exception.BeanInstantiationException;
@@ -10,32 +10,26 @@ import ua.abond.lab4.config.core.exception.BeanInstantiationException;
 import java.lang.reflect.Field;
 import java.util.Arrays;
 
-public class InjectAnnotationBeanPostProcessor implements BeanPostProcessor, Ordered, ApplicationContextAware {
-    private ApplicationContext applicationContext;
+public class InjectAnnotationBeanPostProcessor implements BeanPostProcessor, Ordered {
 
     @Override
-    public Object postProcessBeforeInitialization(Object bean, String beanName) {
+    public Object postProcessBeforeInitialization(ConfigurableBeanFactory factory, Object bean, String beanName) {
         Arrays.stream(bean.getClass().getDeclaredFields()).
                 filter(f -> f.isAnnotationPresent(Inject.class)).
-                forEach(f -> inject(bean, f));
+                forEach(f -> inject(factory, bean, f));
         return bean;
     }
 
-    private void inject(Object bean, Field f) {
+    private void inject(BeanFactory factory, Object bean, Field f) {
         if (!f.isAccessible()) {
             f.setAccessible(true);
         }
-        Object injectable = applicationContext.getBean(f.getType());
+        Object injectable = factory.getBean(f.getType());
         try {
             f.set(bean, injectable);
         } catch (IllegalAccessException e) {
             throw new BeanInstantiationException("Failed to inject " + injectable + " to " + bean);
         }
-    }
-
-    @Override
-    public void setApplicationContext(ApplicationContext applicationContext) {
-        this.applicationContext = applicationContext;
     }
 
     @Override
