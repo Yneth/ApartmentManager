0c47cd34111124364401f145e78943402aa908bd
Anthony <zaknofein2@gmail.com>
2016-11-28 21:14:10 +0200

    As well as the previous commit changed inteface of BeanConstructor

--- src/main/java/ua/abond/lab4/config/core/BeanConstructor.java
+++ src/main/java/ua/abond/lab4/config/core/BeanConstructor.java
@@ -3,6 +3,6 @@ package ua.abond.lab4.config.core;
 import ua.abond.lab4.config.core.bean.BeanDefinition;
 
 public interface BeanConstructor {
-    boolean canCreate(ApplicationContext context, String bean, BeanDefinition beanDefinition);
-    Object create(ApplicationContext context, String bean, BeanDefinition beanDefinition);
+    boolean canCreate(ConfigurableBeanFactory context, String bean, BeanDefinition beanDefinition);
+    Object create(ConfigurableBeanFactory context, String bean, BeanDefinition beanDefinition);
 }
