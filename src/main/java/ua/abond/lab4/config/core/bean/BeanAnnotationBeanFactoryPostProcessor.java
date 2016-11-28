8b3f097216cf0c00a616178ac5dca1853a75564e
Anthony <zaknofein2@gmail.com>
2016-11-28 21:13:05 +0200

    Implemented BeanAnnotationBeanFactoryPostProcessor

--- src/main/java/ua/abond/lab4/config/core/bean/BeanAnnotationBeanFactoryPostProcessor.java
+++ src/main/java/ua/abond/lab4/config/core/bean/BeanAnnotationBeanFactoryPostProcessor.java
@@ -1,27 +1,35 @@
 package ua.abond.lab4.config.core.bean;
 
-import ua.abond.lab4.config.core.ApplicationContext;
+import ua.abond.lab4.config.core.BeanDefinitionRegistry;
 import ua.abond.lab4.config.core.BeanFactoryPostProcessor;
+import ua.abond.lab4.config.core.ConfigurableBeanFactory;
+import ua.abond.lab4.config.core.annotation.Bean;
+import ua.abond.lab4.config.core.exception.BeanInstantiationException;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
 
 public class BeanAnnotationBeanFactoryPostProcessor implements BeanFactoryPostProcessor {
 
-//    @Override
-//    public void postProcess(BeanDefinition beanDefinition, ApplicationContext context) {
-//        Arrays.stream(beanDefinition.getType().getMethods()).
-//                filter(m -> m.isAnnotationPresent(Bean.class)).
-//                map(this::toBeanDefinition);
-//
-//    }
-//
-//    private BeanDefinition toBeanDefinition(Method method) {
-//        if (method.getParameterCount() > 0) {
-//            throw new BeanInstantiationException("Method should not contain arguments. ");
-//        }
-//        return new BeanDefinition(method.getReturnType());
-//    }
-
     @Override
-    public void postProcess(ApplicationContext context) {
+    public void postProcess(ConfigurableBeanFactory context) {
+        BeanDefinitionRegistry registry = context.getBean(BeanDefinitionRegistry.class);
 
+        context.getBeanDefinitionNames().stream().
+                map(context::getBeanDefinition).
+                map(BeanDefinition::getType).
+                map(Class::getDeclaredMethods).
+                flatMap(Arrays::stream).
+                filter(method -> method.isAnnotationPresent(Bean.class)).
+                map(this::toBeanDefinition).
+                forEach(registry::register);
     }
+
+    private BeanDefinition toBeanDefinition(Method method) {
+        if (method.getParameterCount() > 0) {
+            throw new BeanInstantiationException("Method should not contain arguments. ");
+        }
+        return new BeanDefinition(method.getReturnType(), method, method.getDeclaringClass());
+    }
+
 }
