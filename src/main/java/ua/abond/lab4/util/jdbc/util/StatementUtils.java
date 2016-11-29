78a56e1a4a3a576bf2611282dac34a66af2b8d32
Anthony <zaknofein2@gmail.com>
2016-11-30 00:15:29 +0200

    Moved statement and connection utils

---
+++ src/main/java/ua/abond/lab4/util/jdbc/util/StatementUtils.java
@@ -0,0 +1,18 @@
+package ua.abond.lab4.util.jdbc.util;
+
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public final class StatementUtils {
+
+    private StatementUtils() {
+    }
+
+    public static void releaseStatement(Statement statement)
+            throws SQLException {
+        if (statement == null) {
+            return;
+        }
+        statement.close();
+    }
+}
