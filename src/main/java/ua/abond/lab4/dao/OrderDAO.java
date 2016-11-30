1635d95337e5f5750e46606ed1f3c8534f8de8af
Anthony <zaknofein2@gmail.com>
2016-12-01 01:02:33 +0200

    Added list method to OrderDAO

--- src/main/java/ua/abond/lab4/dao/OrderDAO.java
+++ src/main/java/ua/abond/lab4/dao/OrderDAO.java
@@ -6,6 +6,6 @@ import ua.abond.lab4.domain.Order;
 import java.util.List;
 
 public interface OrderDAO extends DAO<Order> {
-    List<Order> paginate(Pageable pageable);
+    List<Order> list(Pageable pageable);
     List<Order> getUserOrders(Long id);
 }
