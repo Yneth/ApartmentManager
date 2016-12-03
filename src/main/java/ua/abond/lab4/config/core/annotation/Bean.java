f5ac6b5d160ce8889414a93e5b7db8df76299e6c
Anthony <zaknofein2@gmail.com>
2016-12-03 14:10:41 +0200

    Removed unnecessary annotation

--- src/main/java/ua/abond/lab4/config/core/annotation/Bean.java
+++ src/main/java/ua/abond/lab4/config/core/annotation/Bean.java
@@ -2,7 +2,6 @@ package ua.abond.lab4.config.core.annotation;
 
 import java.lang.annotation.*;
 
-@Documented
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Bean {
