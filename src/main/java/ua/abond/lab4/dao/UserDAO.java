e552ba1a8ca0b28f09c1b9fdb6d7faf6a08576fa
Anthony <zaknofein2@gmail.com>
2016-12-05 00:55:13 +0200

    reject button now do not appear  when request is not in created state

--- src/main/java/ua/abond/lab4/dao/UserDAO.java
+++ src/main/java/ua/abond/lab4/dao/UserDAO.java
@@ -1,9 +1,13 @@
 package ua.abond.lab4.dao;
 
+import ua.abond.lab4.config.core.web.support.Page;
+import ua.abond.lab4.config.core.web.support.Pageable;
 import ua.abond.lab4.domain.User;
 
 import java.util.Optional;
 
 public interface UserDAO extends DAO<User> {
     Optional<User> getByLogin(String name);
+
+    Page<User> list(Pageable pageable, Long authId);
 }
