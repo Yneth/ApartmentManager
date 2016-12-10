10a98529f1dbab1721cb41072ac43c768902dc26
Anthony <zaknofein2@gmail.com>
2016-12-10 20:57:45 +0200

    Refactor exception handling

--- src/main/java/ua/abond/lab4/web/ApplicationExceptionController.java
+++ src/main/java/ua/abond/lab4/web/ApplicationExceptionController.java
@@ -8,61 +8,68 @@ import ua.abond.lab4.config.core.web.method.HandlerMethodInfo;
 import ua.abond.lab4.config.core.web.support.RequestMethod;
 import ua.abond.lab4.service.exception.*;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.lang.reflect.Method;
 import java.util.Collections;
+import java.util.List;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
 @ExceptionController
 public class ApplicationExceptionController {
 
+    @ExceptionHandler(ValidationException.class)
+    public HandlerMethodInfo handleValidationException(ExceptionHandlerData data) {
+        ValidationException exception = (ValidationException) data.getException();
+        List<String> errors = exception.getErrors();
+        data.getRequest().setAttribute("errors", errors);
+        return getForward(data);
+    }
+
     @ExceptionHandler(ResourceNotFoundException.class)
     public void handleResourceNotFoundException(ExceptionHandlerData data)
-            throws IOException {
+            throws Exception {
         data.getResponse().sendError(HttpServletResponse.SC_NOT_FOUND);
     }
 
     @ExceptionHandler(RequestConfirmException.class)
     public HandlerMethodInfo handleRequestConfirmException(ExceptionHandlerData data)
-            throws IOException, ServletException {
+            throws Exception {
         localizedHandle("request.error.confirm", data);
         return getForward(data);
     }
 
     @ExceptionHandler(ResourceAlreadyExistsException.class)
     public HandlerMethodInfo handleResourceAlreadyExistsException(ExceptionHandlerData data)
-            throws IOException, ServletException {
+            throws Exception {
         localizedHandle("error.already.exists", data);
         return getForward(data);
     }
 
     @ExceptionHandler(LoginIsAlreadyTakenException.class)
     public HandlerMethodInfo handleLoginIsAlreadyTakenException(ExceptionHandlerData data)
-            throws IOException, ServletException {
+            throws Exception {
         localizedHandle("login.error.login.taken", data);
         return getForward(data);
     }
 
     @ExceptionHandler(OrderAlreadyPayedException.class)
     public HandlerMethodInfo handleOrderAlreadyPayedException(ExceptionHandlerData data)
-            throws IOException, ServletException {
+            throws Exception {
         localizedHandle("order.error.payed", data);
         return getForward(data);
     }
 
     @ExceptionHandler(RejectRequestException.class)
     public HandlerMethodInfo handleRejectRequestException(ExceptionHandlerData data)
-            throws ServletException, IOException {
+            throws Exception {
         localizedHandle("request.error.reject", data);
         return getForward(data);
     }
 
     private void localizedHandle(String errorKey, ExceptionHandlerData data)
-            throws IOException, ServletException {
+            throws Exception {
         HandlerMethodInfo forward = getForward(data);
         if (forward != null) {
             HttpServletRequest req = data.getRequest();
--- src/test/java/ua/abond/lab4/web/ApplicationExceptionControllerTest.java
+++ src/test/java/ua/abond/lab4/web/ApplicationExceptionControllerTest.java
@@ -1,5 +1,25 @@
 package ua.abond.lab4.web;
 
-public class ApplicationExceptionControllerTest extends ControllerTest {
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.runners.MockitoJUnitRunner;
+import ua.abond.lab4.config.core.web.method.ExceptionHandlerData;
 
+@RunWith(MockitoJUnitRunner.class)
+public class ApplicationExceptionControllerTest extends ControllerTest {
+    private ApplicationExceptionController controller;
+    @InjectMocks
+    private ExceptionHandlerData handlerData;
+
+    @Before
+    public void setUp() {
+        controller = new ApplicationExceptionController();
+    }
+
+    @Test
+    public void testHandleResourceNotFound() {
+
+    }
 }
