67bb5f1088b1df9946720d59bc0e99471bba3b16
Anthony <zaknofein2@gmail.com>
2016-12-11 16:04:39 +0200

    Add possibility to get property from environment

--- src/main/java/ua/abond/lab4/config/DatabaseConfig.java
+++ src/main/java/ua/abond/lab4/config/DatabaseConfig.java
@@ -5,6 +5,7 @@ import ua.abond.lab4.config.core.annotation.*;
 import ua.abond.lab4.config.core.infrastructure.TransactionManager;
 
 import javax.sql.DataSource;
+import java.util.Optional;
 
 @Component
 @Prop("db.properties")
@@ -22,9 +23,9 @@ public class DatabaseConfig {
     @Bean
     public DataSource getDataSource() {
         PGPoolingDataSource dataSource = new PGPoolingDataSource();
-        dataSource.setUrl(url);
-        dataSource.setUser(username);
-        dataSource.setPassword(password);
+        dataSource.setUrl(getEnvProperty("DATABASE_URL").orElse(url));
+        dataSource.setUser(getEnvProperty("DATABASE_USERNAME").orElse(username));
+        dataSource.setPassword(getEnvProperty("DATABASE_PASSWORD").orElse(password));
         dataSource.setInitialConnections(10);
         dataSource.setMaxConnections(50);
         return dataSource;
@@ -33,5 +34,9 @@ public class DatabaseConfig {
     @Bean
     public TransactionManager transactionManager() {
         return new TransactionManager();
+    }
+
+    private Optional<String> getEnvProperty(String prop) {
+        return Optional.ofNullable(System.getenv().get(prop));
     }
 }
