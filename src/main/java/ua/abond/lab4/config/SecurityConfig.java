866131efd8fc45ea5d6c5476353a872945592c49
Anthony <zaknofein2@gmail.com>
2016-12-14 21:57:38 +0200

    Add password encoder

--- src/main/java/ua/abond/lab4/config/SecurityConfig.java
+++ src/main/java/ua/abond/lab4/config/SecurityConfig.java
@@ -1,8 +1,15 @@
 package ua.abond.lab4.config;
 
+import ua.abond.lab4.core.annotation.Bean;
 import ua.abond.lab4.core.annotation.Component;
+import ua.abond.lab4.security.BCryptPasswordEncoder;
+import ua.abond.lab4.security.PasswordEncoder;
 
 @Component
 public class SecurityConfig {
 
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
