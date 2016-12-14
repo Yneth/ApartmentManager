bedd9ad1f0394089f7676b5d3b280ee7af356049
Anthony <zaknofein2@gmail.com>
2016-12-14 13:45:41 +0200

    Add exception for KeyHolder

---
+++ src/main/java/ua/abond/lab4/util/jdbc/exception/InvalidKeyTypeException.java
@@ -0,0 +1,8 @@
+package ua.abond.lab4.util.jdbc.exception;
+
+public class InvalidKeyTypeException extends RuntimeException {
+
+    public InvalidKeyTypeException(String message) {
+        super(message);
+    }
+}
